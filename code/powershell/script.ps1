1a7bba9755e1e00e758890a2c8c688bcb95928a8
Chara Anis <chara@et.esiea.fr>
2024-12-02 13:53:17 +0100

    proto compileur

---
+++ code/powershell/script.ps1
@@ -0,0 +1,33 @@
+Param(
+    [string]$customDistPath
+)
+
+# Chemin vers PyInstaller
+$pyinstaller = "C:\Users\user\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\pyinstaller.exe"
+
+# Chemin du script Python à compiler
+$pythonScript = "C:\Users\user\Documents\COURS\C#\Projet\bricks-studio\code\python\test.py"
+
+# Compilation avec PyInstaller
+& $pyinstaller --onefile --windowed --distpath $customDistPath $pythonScript
+
+# # Chemin du Bureau
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+
+# # Nom du fichier exécutable généré par PyInstaller (dans le dossier dist personnalisé)
+$executableName = "test.exe"  # Nom basé sur le script original
+$executablePath = Join-Path -Path $customDistPath -ChildPath $executableName
+
+# # Chemin pour le raccourci
+$shortcutPath = Join-Path -Path $desktopPath -ChildPath "test.lnk"
+
+# Création du raccourci avec Windows Script Host
+$WshShell = New-Object -ComObject WScript.Shell
+$shortcut = $WshShell.CreateShortcut($shortcutPath)
+$shortcut.TargetPath = $executablePath
+$shortcut.IconLocation = $executablePath  # Utilise l'icône de l'exécutable
+$shortcut.WindowStyle = 1                 # Fenêtre normale
+$shortcut.Description = "Raccourci pour test.exe"
+$shortcut.Save()
+
+Write-Host "Compilation terminée, exécutable généré dans '$customDistPath' et raccourci créé sur le Bureau."
---
+++ doc/UML_compilateur.md
@@ -0,0 +1,7 @@
+    input :
+    - path where to store game
+    - path of the project
+    - json_string"
+
+    TODO : 
+    - changer le workingDir pour le projectDir
