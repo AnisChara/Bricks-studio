0cfc14193fe78135b35d9545dc48e5e10ebb588f
Isaac LEVY <isaaclda822@gmail.com>
2024-12-03 09:41:55 +0100

    Creation de l'interface WPF

---
+++ Bricks_Interfaces/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Bricks_Interfaces
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
