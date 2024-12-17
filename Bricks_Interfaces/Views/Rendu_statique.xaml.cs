dd5d2ae1b73bf8b25e3fde67d567064b2d88bf0d
Isaac LEVY <isaaclda822@gmail.com>
2024-12-17 13:49:39 +0100

    git ignore config

---
+++ Bricks_Interfaces/Views/Rendu_statique.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bricks_Interfaces.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Rendu_statique.xaml
+    /// </summary>
+    public partial class Rendu_statique : UserControl
+    {
+        public Rendu_statique()
+        {
+            InitializeComponent();
+        }
+    }
+}
