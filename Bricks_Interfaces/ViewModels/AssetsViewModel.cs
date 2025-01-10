0d65be48d38f8e295b7d7d10921f2c55bfa1ad7e
alpha <alphdiallo@et.esiea.fr>
2025-01-10 09:31:23 +0100

    Methode addennemy et addZone

--- Bricks_Interfaces/ViewModels/AssetsViewModel.cs
+++ Bricks_Interfaces/ViewModels/AssetsViewModel.cs
@@ -12,10 +12,15 @@ namespace Bricks_Interfaces.ViewModels
     {
         public ICommand AddStructure {  get; set; }
         public ICommand AddObstacle {  get; set; }
+        public ICommand AddEnnemy { get; set; }
+        public ICommand AddZone { get; set; }
+
         public AssetsViewModel() 
         {
             AddStructure = new RelayCommand(Add_structure);
             AddObstacle = new RelayCommand(Add_obstacle);
+            AddEnnemy = new RelayCommand(Add_Ennemy);
+            AddZone = new RelayCommand(Add_Zone);
         }
         
         private void Add_structure(object parameter) 
@@ -24,7 +29,17 @@ namespace Bricks_Interfaces.ViewModels
         }
         private void Add_obstacle(object parameter) 
         {
-            MessageBox.Show("Obstacle ajoutée");
+            MessageBox.Show("Obstacle ajouté");
+        }
+
+        private void Add_Ennemy(object parameter)
+        {
+            MessageBox.Show("Ennemy ajouté");
+        }
+
+        private void Add_Zone(object parameter)
+        {
+            MessageBox.Show("Zone ajoutée");
         }
     }
 }
