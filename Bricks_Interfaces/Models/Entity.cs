687b8ec3693bedc9cd4e5e131ffcd5fb5e7c8919
alpha <alphdiallo@et.esiea.fr>
2025-01-10 14:38:45 +0100

    classe entity et methode entity

--- Bricks_Interfaces/Models/Entity.cs
+++ Bricks_Interfaces/Models/Entity.cs
@@ -8,8 +8,50 @@ namespace Bricks_Interfaces.Models
 {
     public class Entity
     {
-        public string Coordonnee { get; set; }
-        public double Variable { get; set; }
+    public string type { get; set; }
+
+    public string Id { get; set; }
+
+    public int x { get; set; }
+
+     public int y { get; set; }
+
+    public int width { get; set; }
+
+    public int height { get; set; }
+
+    public int speed { get; set; }
+
+    public bool is_collidable { get; set; }
+
+     public string shape {  get; set; }
+
+    public int  weight {  get; set; } 
+
+    public bool render {  get; set; }
+
+    public bool has_weapon {  get; set; }
+
+    public int? max_health { get; set; }
+
+        public Entity (string type, string Id, int x, int y, int width,  int height, int speed, bool is_collidable,  string shape, int weight, bool render, bool has_weapon, int? max_health)
+        {
+            this.type = type;
+            this.Id = Id;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.speed = speed;
+            this.is_collidable = is_collidable;
+            this.shape = shape;
+            this.weight = weight;
+            this.render = render;
+            this.has_weapon = has_weapon;
+            this.max_health = max_health;
+        }
     }
+
+    
 
 }
--- Bricks_Interfaces/ViewModels/AssetsViewModel.cs
+++ Bricks_Interfaces/ViewModels/AssetsViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Bricks_Interfaces.Models;
+using Bricks_Interfaces.Views.AllOnglets;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -25,21 +29,121 @@ namespace Bricks_Interfaces.ViewModels
         
         private void Add_structure(object parameter) 
         {
-            MessageBox.Show("Structure ajoutée");
+            string json = System.IO.File.ReadAllText("../../../Entity.json");
+            ObservableCollection<Entity> Entités = new ObservableCollection<Entity>(JsonSerializer.Deserialize<List<Entity>>(json));
+
+                var newEntity = new Entity(
+                    type: "structure",
+                    Id: Guid.NewGuid().ToString(),
+                    x: 100,
+                    y: 200,
+                    width: 50,
+                    height: 50,
+                    speed: 5,
+                    is_collidable: true,
+                    shape: "square",
+                    weight: 10,
+                    render: false,
+                    has_weapon: false,
+                    max_health: 100
+    );
+
+            Entités.Add(newEntity);
+
+
+            json = JsonSerializer.Serialize(Entités, new JsonSerializerOptions { WriteIndented = true });
+            System.IO.File.WriteAllText("../../../Entity.json", json);
+
+            MessageBox.Show("Nouvelle entité ajoutée avec succès !");
         }
         private void Add_obstacle(object parameter) 
         {
-            MessageBox.Show("Obstacle ajouté");
+            string json = System.IO.File.ReadAllText("../../../Entity.json");
+            ObservableCollection<Entity> Entités = new ObservableCollection<Entity>(JsonSerializer.Deserialize<List<Entity>>(json));
+
+            var newEntity = new Entity(
+                type: "obstacle",
+                Id: Guid.NewGuid().ToString(),
+                x: 100,
+                y: 200,
+                width: 50,
+                height: 50,
+                speed: 5,
+                is_collidable: true,
+                shape: "square",
+                weight: 10,
+                render: false,
+                has_weapon: false,
+                max_health: 100
+);
+
+            Entités.Add(newEntity);
+
+
+            json = JsonSerializer.Serialize(Entités, new JsonSerializerOptions { WriteIndented = true });
+            System.IO.File.WriteAllText("../../../Entity.json", json);
+
+            MessageBox.Show("Nouvelle entité ajoutée avec succès !");
         }
 
         private void Add_Ennemy(object parameter)
         {
-            MessageBox.Show("Ennemy ajouté");
+            string json = System.IO.File.ReadAllText("../../../Entity.json");
+            ObservableCollection<Entity> Entités = new ObservableCollection<Entity>(JsonSerializer.Deserialize<List<Entity>>(json));
+
+            var newEntity = new Entity(
+                type: "ennemy",
+                Id: Guid.NewGuid().ToString(),
+                x: 100,
+                y: 200,
+                width: 50,
+                height: 50,
+                speed: 5,
+                is_collidable: true,
+                shape: "square",
+                weight: 10,
+                render: false,
+                has_weapon: false,
+                max_health: 100
+);
+
+            Entités.Add(newEntity);
+
+
+            json = JsonSerializer.Serialize(Entités, new JsonSerializerOptions { WriteIndented = true });
+            System.IO.File.WriteAllText("../../../Entity.json", json);
+
+            MessageBox.Show("Nouvelle entité ajoutée avec succès !");
         }
 
         private void Add_Zone(object parameter)
         {
-            MessageBox.Show("Zone ajoutée");
+            string json = System.IO.File.ReadAllText("../../../Entity.json");
+            ObservableCollection<Entity> Entités = new ObservableCollection<Entity>(JsonSerializer.Deserialize<List<Entity>>(json));
+
+            var newEntity = new Entity(
+                type: "zone",
+                Id: Guid.NewGuid().ToString(),
+                x: 100,
+                y: 200,
+                width: 50,
+                height: 50,
+                speed: 5,
+                is_collidable: true,
+                shape: "square",
+                weight: 10,
+                render: false,
+                has_weapon: false,
+                max_health: 100
+);
+
+            Entités.Add(newEntity);
+
+
+            json = JsonSerializer.Serialize(Entités, new JsonSerializerOptions { WriteIndented = true });
+            System.IO.File.WriteAllText("../../../Entity.json", json);
+
+            MessageBox.Show("Nouvelle entité ajoutée avec succès !");
         }
     }
 }
