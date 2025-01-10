a46a345d9d7c96bd27102be79752dcc5ff9c28b4
Isaac LEVY <isaaclda822@gmail.com>
2025-01-10 09:02:35 +0100

    Ajout de boutons dans l interface Assets, création du AssetsViewModel et des méthodes AddStructure et AddObstacle

---
+++ Bricks_Interfaces/ViewModels/AssetsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Bricks_Interfaces.ViewModels
+{
+    internal class AssetsViewModel : BaseNotifyPropertyChanged
+    {
+        public ICommand AddStructure {  get; set; }
+        public ICommand AddObstacle {  get; set; }
+        public AssetsViewModel() 
+        {
+            AddStructure = new RelayCommand(Add_structure);
+            AddObstacle = new RelayCommand(Add_obstacle);
+        }
+        
+        private void Add_structure(object parameter) 
+        {
+            MessageBox.Show("Structure ajoutée");
+        }
+        private void Add_obstacle(object parameter) 
+        {
+            MessageBox.Show("Obstacle ajoutée");
+        }
+    }
+}
