d192cf0f3f080c859cd36f68a2afcfba9f115b6e
Chara Anis <chara@et.esiea.fr>
2025-01-26 13:18:02 +0100

    dm

--- Bricks_Interfaces/Views/AllOnglets/Nodes.xaml.cs
+++ Bricks_Interfaces/Views/AllOnglets/Nodes.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Bricks_Interfaces.Models;
 using Bricks_Interfaces.ViewModels;
 
 namespace Bricks_Interfaces.Views.AllOnglets
@@ -25,6 +26,44 @@ namespace Bricks_Interfaces.Views.AllOnglets
         {
             InitializeComponent();
             this.DataContext = new NodeViewModel();
+        }
+
+        private void Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                var node = button.DataContext as Node; 
+                if (node != null)
+                {
+                    ((NodeViewModel)this.DataContext).StartDrag(node);
+                    return;
+                }
+                var meca = button.DataContext as Mecanique;
+                if (meca != null)
+                {
+                    ((NodeViewModel)this.DataContext).StartDrag(meca);
+                    return;
+                }
+                var decl = button.DataContext as Declencheur;
+                if (decl != null)
+                {
+                    ((NodeViewModel)this.DataContext).StartDrag(decl);
+                    return;
+                }
+            }
+        }
+
+        private void Button_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            Grid sen = (Grid)sender;
+            Point _startPoint = e.GetPosition(sender as Grid);
+
+            ((NodeViewModel)this.DataContext).ActualiseDrag(_startPoint, sen.ActualWidth, sen.ActualHeight);
+        }
+
+        private void Button_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ((NodeViewModel)this.DataContext).StopDrag();
         }
     }
 }
