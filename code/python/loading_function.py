da42b3b70f0303a4c85a80e6464b6d42fc45fd0f
Chara Anis <chara@et.esiea.fr>
2025-02-04 16:18:56 +0100

    dm

--- code/python/loading_function.py
+++ code/python/loading_function.py
@@ -12,20 +12,20 @@ def instance_entities(levels,level):
     for entity in levels[level]["Entities"]:
 
         if entity['type'] == 'structure':
-            instance = Structure(entity['id'], entity['x']*1.71, entity['y']*2.07, entity['width']*1.71, entity['height']*2.07,entity['speed'],entity['is_collidable'] , entity['shape'],entity['weight'], entity['render'],entity['image'] )
+            instance = Structure(entity['id'], entity['x']*1.7297, entity['y']*1.925, entity['width']*1.7297, entity['height']*1.925,entity['speed'],entity['is_collidable'] , entity['shape'],entity['weight'], entity['render'],entity['image'] )
             Game.entities.append(instance)
         elif entity['type'] == 'offensive_structure':
-            instance = Offensive_Structure(entity['id'], entity['x']*1.71, entity['y']*2.07, entity['width']*1.71, entity['height']*2.07,entity['speed'],entity['is_collidable'] , entity['shape'],entity['weight'], entity['render'],entity['damage'],entity['image'])
+            instance = Offensive_Structure(entity['id'], entity['x']*1.7297, entity['y']*1.925, entity['width']*1.7297, entity['height']*1.925,entity['speed'],entity['is_collidable'] , entity['shape'],entity['weight'], entity['render'],entity['damage'],entity['image'])
             Game.entities.append(instance)
         elif entity['type'] == 'offensive_entity':
-            instance = Offensive_Entity(entity['id'], entity['x']*1.71, entity['y']*2.07, entity['width']*1.71, entity['height']*2.07,entity["max_health"],entity['speed'],entity['is_collidable'] , entity['shape'],entity['weight'], entity['render'],entity['damage'],entity['image'],entity['Track'])
+            instance = Offensive_Entity(entity['id'], entity['x']*1.7297, entity['y']*1.925, entity['width']*1.7297, entity['height']*1.925,entity["max_health"],entity['speed'],entity['is_collidable'] , entity['shape'],entity['weight'], entity['render'],entity['damage'],entity['image'],entity['Track'])
             instance.image = pygame.transform.flip(instance.image,True,False)
             Game.entities.append(instance)
         elif entity['type'] == 'zone':
-            instance = Structure(entity['id'], entity['x']*1.71, entity['y']*2.07, entity['width']*1.71, entity['height']*2.07,entity['speed'],entity['is_collidable'] , entity['shape'],entity['weight'], entity['render'],entity['image'],True)
+            instance = Structure(entity['id'], entity['x']*1.7297, entity['y']*1.925, entity['width']*1.7297, entity['height']*1.925,entity['speed'],entity['is_collidable'] , entity['shape'],entity['weight'], entity['render'],entity['image'],True)
             Game.entities.append(instance)
         elif entity['type'] == 'Player':
-            Player = Pawn(entity['id'], entity['x']*1.71, entity['y']*2.07, entity['width']*1.71, entity['height']*2.07,entity['max_health'],entity['speed'],entity['is_collidable'] , entity['shape'],entity['weight'],entity['image'])
+            Player = Pawn(entity['id'], entity['x']*1.7297, entity['y']*1.925, entity['width']*1.7297, entity['height']*1.925,entity['max_health'],entity['speed'],entity['is_collidable'] , entity['shape'],entity['weight'],entity['image'])
             Game.entities.append(Player)
             Player.set_weapon()
         if entity["has_weapon"]:
